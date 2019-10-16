5afd9b2ed39d86d3f11b0f63adfef5975b66adc6
Nathaniel Mosco <natemosco@gmail.com>
2019-10-16 16:31:39 -0400

    Project setup - added useState to main app.js

---
+++ team-builder/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [list, setList] = useState({});
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
