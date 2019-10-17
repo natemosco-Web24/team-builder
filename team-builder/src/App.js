2f5176b05a6cad169a3407c14bb53338027ce171
Nathaniel Mosco <natemosco@gmail.com>
2019-10-16 21:01:17 -0400

    form in place. adding onclick onsubmit handlers employee card is in place require looping through object to create cards still.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import Form from "./components/Form";
+import EmployeeCard from "./components/EmployeeCard";
+
+
 function App() {
   const [list, setList] = useState({});
   return (
     <div className="App">
-
+      <Form setList={setList} list={list}></Form>
+      for (employee in list){
+        <EmployeeCard employee={list[employee]}></EmployeeCard>
+      }
     </div>
   );
 }
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+export default function Form(props) {
+    const [employee, setEmployee] = useState({ name: "", email: "", role: "", employeeid: "" });
+    const [key, setKey] = useState({})
+
+    const onChange = (event) => {
+        setEmployee(...employee, ...{ [event.target.id]: event.target.value })
+    }
+    const onChangeID = (event) => {
+        setKey({ ...key, ...{ [event.target.id]: event.target.value } })
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        props.setList({ ...props.list, ...{ [key]: { ...employee } } })
+        setKey({})
+        setEmployee({ name: "", email: "", role: "", employeeid: "" })
+    }
+
+    return (
+        <div>
+            <form onSubmit={(event) => { onSubmit(event) }}>
+                <label htmlFor="name">Name:</label>
+                <input name="name" id="name" placeholder="name" type="text" onChange={(event) => { onChange(event) }} />
+
+                <label htmlFor="email">Email:</label>
+                <input name="email" id="email" placeholder="person@gmail.com" type="email" onChange={(event) => { onChange(event) }} />
+
+                <label htmlFor="role">Role:</label>
+                <input name="role" id="role" type="text" onChange={(event) => { onChange(event) }} />
+
+                <label htmlFor="employeeid">Employee ID:</label>
+                <input name="employeeid" id="employeeid" type="password" onChange={(event) => { onChange(event) }} />
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
